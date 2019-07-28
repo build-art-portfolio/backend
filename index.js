ca3116404a556405a10058348d287500bd3db11b
springercass <casprin2@gmail.com>
2019-07-28 09:52:14 -0500

    debugging

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
 const server = require("./api/server");
+const port = process.env.PORT || 5000;
 
-server.listen(5000, console.log("server listening on port 5000"));
+server.listen(port, console.log(`server listening on port ${port}`));
